f22b217b707b16a42befdacfe1c47c0afacfb818
pyraxo <pyraxo@pyraxo.moe>
2023-06-22 03:32:31 +0800

    Fix spline render

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import uuid from "react-uuid";
-import cubicleLogo from "../assets/cubicle_logo.png"
+import cubicleLogo from "assets/cubicle_logo.png";
 
-import useLocalStorage from "../hooks/useLocalStorage";
 import { fetchValue, saveValue } from "../services/Firebase";
 import { TextField, Button } from "@mui/material";
 
@@ -11,7 +10,6 @@ import "./Login.css";
 
 const Login = () => {
   const { state } = useLocation();
-  const { from } = state || {};
   const navigate = useNavigate();
 
   const [username, setUsername] = useState("");
@@ -20,7 +18,7 @@ const Login = () => {
 
   const handleTogglePage = () => {
     setIsLogin((prevState) => !prevState);
-  };  
+  };
 
   const handleSignIn = async () => {
     const data = await fetchValue(`users/${username}`);
@@ -42,92 +40,96 @@ const Login = () => {
       await saveValue(dbpath, { password, userId });
       localStorage.setItem("userData", JSON.stringify({ username, userId }));
       navigate("/", { replace: true });
-      console.log("inv");
     }
-  }
+  };
 
   return (
     <div className="login_container">
-    <div className="login">
-      <h1>Cubicle</h1>
-      <div>
+      <div className="login">
+        <h1>Cubicle</h1>
         <div>
-          <img src={cubicleLogo} alt="" id="cubicle_logo" /> 
-          <h2 className="login_header1">Bridge the Distance <br></br>
-          Revive the Buzz</h2>
-          {/* <h2 className="login_header2">Revive the Buzz</h2> */}
-
-          {isLogin ? (
-          // Render login section
           <div>
-            <div className="field_container">
-              <TextField
-                id="outlined-basic"
-                label="Username"
-                variant="outlined"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="field_container">
-              <TextField
-                id="outlined-basic"
-                label="Password"
-                variant="outlined"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onKeyDown={(e) => e.key === "Enter" && handleSignIn()}
-              />
-            </div>
-            <div className="button_container">
-              <Button variant="contained" onClick={handleSignIn}>
-                Login
-              </Button>
-            </div>
-          </div>
-        ) : (
-          // Render signup section
-          <div>
-            <div className="field_container">
-              <TextField
-                id="outlined-basic"
-                label="New Username"
-                variant="outlined"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
+            <img src={cubicleLogo} alt="" id="cubicle_logo" />
+            <h2 className="login_header1">
+              Bridge the Distance <br></br>
+              Revive the Buzz
+            </h2>
+            {/* <h2 className="login_header2">Revive the Buzz</h2> */}
 
-            <div className="field_container">
-              <TextField
-                id="outlined-basic"
-                label="New Password"
-                variant="outlined"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onKeyDown={(e) => e.key === "Enter" && handleSignUp()}
-              />
-            </div>
-            <div className="button_container">
-              <Button variant="contained" onClick={handleSignUp}>
-                Sign up
-              </Button>
-            </div>
-          </div>
-        )}
-          <div className="button_container">
-            <Button variant="contained" onClick={handleTogglePage}>New here? Click me!</Button>
+            {isLogin ? (
+              // Render login section
+              <div>
+                <div className="field_container">
+                  <TextField
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="field_container">
+                  <TextField
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    onKeyDown={(e) => e.key === "Enter" && handleSignIn()}
+                  />
+                </div>
+                <div className="button_container">
+                  <Button variant="contained" onClick={handleSignIn}>
+                    Login
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              // Render signup section
+              <div>
+                <div className="field_container">
+                  <TextField
+                    id="outlined-basic"
+                    label="New Username"
+                    variant="outlined"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+
+                <div className="field_container">
+                  <TextField
+                    id="outlined-basic"
+                    label="New Password"
+                    variant="outlined"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    onKeyDown={(e) => e.key === "Enter" && handleSignUp()}
+                  />
+                </div>
+                <div className="button_container">
+                  <Button variant="contained" onClick={handleSignUp}>
+                    Sign up
+                  </Button>
+                </div>
+              </div>
+            )}
+            {isLogin ? (
+              <div className="button_container">
+                <Button variant="contained" onClick={handleTogglePage}>
+                  New here? Click me!
+                </Button>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
-
       </div>
-    </div>
     </div>
   );
 };
 
-export default Login; 
-  
-  
+export default Login;
